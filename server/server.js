45d3fd3aae6a1fff7f76ba413344bfd317e439b8
Zaid Azkoul <zaid@Zaids-MacBook-Pro.local>
2025-06-12 18:20:29 -0500

    Getting Ready for Deployment

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ import { fetchDailyAsteroids } from './utils/fetchDailyAsteroids.js'
 
 dotenv.config()
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 const app = express()
 app.use(express.json())
